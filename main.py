7dbe7b3308c966f97c9c45ca403e1f6cfd55d7c7
andeen171 <zeander21@outlook.com.br>
2021-06-15 19:19:53 -0300

    Ver 1.0 complete

--- main.py
+++ main.py
@@ -1,20 +1,81 @@
 import discord
+import pytz
 import json
 from datetime import datetime, time, date, timedelta
 
 client = discord.Client()
 with open('config.json', 'r') as token:
     discord_token = json.load(token)['token']
-first = time(16, 0, 0)
-second = time(16, 50, 0)
-third = time(17, 40, 0)
-pause = time(18, 35, 0)
-forth = time(18, 55, 0)
-fifth = time(19, 45, 0)
-end = time(20, 35, 0)
-hours = [first, second, third, forth, fifth]
+first = time(0, 0, 0)
+second = time(0, 0, 0)
+third = time(0, 0, 0)
+pause = time(0, 0, 0)
+forth = time(0, 0, 0)
+fifth = time(0, 0, 0)
+end = time(0, 0, 0)
+hours = []
 days = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sabado']
 roleId = {'3c2': '853795487513706537', '3a2': '853795394676850728'}
+
+
+def ConvertToUTC(hour):
+    local = pytz.timezone("America/Sao_Paulo")
+    local_dt = local.localize(hour, is_dst=None)
+    return local_dt.astimezone(pytz.utc)
+
+
+def CreateGlobals(data, command, weekDay):
+    global first, second, third, pause, forth, fifth, end, hours
+    dataFiltered = data[command][weekDay]
+
+    temp = datetime.strptime(dataFiltered['1']['time'][0], '%H:%M')
+    first = ConvertToUTC(temp).time()
+
+    temp = datetime.strptime(dataFiltered['1']['time'][1], '%H:%M')
+    second = ConvertToUTC(temp).time()
+
+    temp = datetime.strptime(dataFiltered['2']['time'][1], '%H:%M')
+    third = ConvertToUTC(temp).time()
+
+    temp = datetime.strptime(dataFiltered['3']['time'][1], '%H:%M')
+    pause = ConvertToUTC(temp).time()
+
+    temp = datetime.strptime(dataFiltered['4']['time'][0], '%H:%M')
+    forth = ConvertToUTC(temp).time()
+
+    temp = datetime.strptime(dataFiltered['4']['time'][1], '%H:%M')
+    fifth = ConvertToUTC(temp).time()
+
+    temp = datetime.strptime(dataFiltered['5']['time'][1], '%H:%M')
+    end = ConvertToUTC(temp).time()
+    hours = [first, second, third, forth, fifth]
+
+
+def NextSubject(weekDay, data, command, interval=False):
+    dataFiltred = data[command]
+    if interval:
+        return dataFiltred[weekDay]["4"]['subject']
+    if int(weekDay) < 1 or int(weekDay) >= 5:
+        return dataFiltred['1']['1']['subject']
+    nextDay = int(weekDay) + 1
+    return dataFiltred[str(nextDay)]['1']['subject']
+
+
+def TimeUntil(hour, interval=False, free=False, weekDay=None):
+    dia = date(1, 1, 1)
+    if interval:
+        timeEnd = datetime.combine(dia, forth)
+    elif free:
+        if weekDay == '5':
+            timeEnd = datetime.combine(date(1, 1, 4), first)
+        elif weekDay == '6':
+            timeEnd = datetime.combine(date(1, 1, 3), first)
+        else:
+            timeEnd = datetime.combine(date(1, 1, 2), first)
+    else:
+        timeEnd = datetime.combine(dia, hours[int(hour) - 1])
+    timeNow = datetime.combine(dia, datetime.utcnow().time())
+    return round(timedelta.total_seconds(timeEnd - timeNow) / 60, 1)
 
 
 def ExceptionHandling(message, data):
@@ -30,13 +91,12 @@ def ExceptionHandling(message, data):
     return
 
 
-def CreateEmbed(data, x, command):
+def CreateEmbed(data, x):
     strings = []
     for key, value in data[str(x)].items():
         strings.append(f'{key}° Horário: {value["subject"]} | {value["time"][0]} - {value["time"][1]}')
     return discord.Embed(title=days[int(x)],
-                         description=f'@{command.upper()}\n '
-                                     f'{strings[0]}\n '
+                         description=f'{strings[0]}\n '
                                      f'{strings[1]}\n '
                                      f'{strings[2]}\n '
                                      f'{strings[3]}\n '
@@ -52,7 +112,6 @@ def GetRawFile():
 def GetActualTime():
     timeNow = datetime.utcnow().time()
     # timeNow = time(20, 0, 0)
-    weekday = str(datetime.utcnow().weekday() + 1)
     if first < timeNow < second:
         hour = '1'
     elif second < timeNow < third:
@@ -60,14 +119,14 @@ def GetActualTime():
     elif third < timeNow < pause:
         hour = '3'
     elif pause < timeNow < forth:
-        hour = 'Interval'
+        hour = 'interval'
     elif forth < timeNow < fifth:
         hour = '4'
     elif fifth < timeNow < end:
         hour = '5'
     else:
         hour = 'free'
-    return [weekday, hour]
+    return hour
 
 
 @client.event
@@ -81,54 +140,67 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    data = GetRawFile()
+    weekDay = str(datetime.utcnow().weekday() + 1)
 
     if message.content.startswith('!horario'):
-        data = GetRawFile()
         exception = ExceptionHandling(message, data)
         if exception:
             await message.channel.send(exception)
             return
         command = message.content.split()[1]
+        CreateGlobals(data, command, weekDay)
         dataFiltred = data[command]
         for x in range(1, 6):
-            embedVar = CreateEmbed(dataFiltred, x, command)
+            embedVar = CreateEmbed(dataFiltred, x)
             await message.channel.send(embed=embedVar)
 
     if message.content.startswith('!hoje'):
-        data = GetRawFile()
         exception = ExceptionHandling(message, data)
         if exception:
             await message.channel.send(exception)
             return
         command = message.content.split()[1]
+        CreateGlobals(data, command, weekDay)
         dataFiltred = data[command]
-        timeData = GetActualTime()
-        weekDay = timeData[0]
-        embedVar = CreateEmbed(dataFiltred, weekDay, command)
+        embedVar = CreateEmbed(dataFiltred, weekDay)
         await message.channel.send(embed=embedVar)
 
     if message.content.startswith('!agora'):
-        data = GetRawFile()
         exception = ExceptionHandling(message, data)
         if exception:
             await message.channel.send(exception)
             return
         command = message.content.split()[1]
-        timeData = GetActualTime()
-        weekDay = timeData[0]
-        hour = timeData[1]
+        CreateGlobals(data, command, weekDay)
+        hour = GetActualTime()
         if hour == 'free':
-            await message.channel.send('Nenhuma aula agora!')
+            timeLeft = TimeUntil(hour, free=True, weekDay=weekDay)
+            if timeLeft > 60:
+                timeLeft = str(round(timeLeft / 60, 1)) + ' horas'
+            else:
+                timeLeft = str(timeLeft) + ' minutos'
+            nextSubject = NextSubject(weekDay, data, command)
+            if nextSubject == "Livre":
+                nextSubject = "vaga antes de RAC"
+            else:
+                nextSubject = f'de {nextSubject}'
+            embedVar = discord.Embed(title='Nenhuma aula hoje!',
+                                     description=f'Falta {timeLeft} para a aula {nextSubject} amanhã!',
+                                     color=0xfc03f0)
+            await message.channel.send(embed=embedVar)
             return
         if hour == 'interval':
-            await message.channel.send('Ta no intervalo!')
+            timeLeft = TimeUntil(hour, interval=True)
+            nextSubject = NextSubject(weekDay, data, command, interval=True)
+            embedVar = discord.Embed(title='Ta no intervalo',
+                                     description=f'Falta {timeLeft} minutos para a aula de {nextSubject} ainda!',
+                                     color=0xfc03f0)
+            await message.channel.send(embed=embedVar)
             return
         subject = data[command][weekDay][hour]['subject']
         startEnd = data[command][weekDay][hour]['time']
-        dia = date(1, 1, 1)
-        timeEnd = datetime.combine(dia, hours[int(hour) - 1])
-        timeNow = datetime.combine(dia, datetime.utcnow().time())
-        timeLeft = round(timedelta.total_seconds(timeNow - timeEnd) / 60, 1)
+        timeLeft = TimeUntil(hour)
         embedVar = discord.Embed(title=f'Ta tendo aula de {subject} agora, corre lá!',
                                  description=f'Começou {startEnd[0]} e vai terminar {startEnd[1]}\n '
                                              f'falta {timeLeft} minutos para acabar!\n'
